22b562d7960ba550444f14152fe7c2867a8eef4f
Alex <alexandre.crooij@students.hevs.ch>
2018-10-19 10:19:05 +0200

    Inclusion of Membres de Honeur

--- chanterproj/routes/index.js
+++ chanterproj/routes/index.js
@@ -85,6 +85,7 @@ exports = module.exports = function (app) {
 	app.get('/events', routes.views.events);
 	app.get('/comitee', routes.views.comitee);
 	app.get('/commission', routes.views.commission);
+	app.get('/honor', routes.views.honor);
 	app.get('/choirfonds', routes.views.choirfonds);
 	app.get('/newsletter', routes.views.newsletter);
 	app.get('/choir', routes.views.choir);
